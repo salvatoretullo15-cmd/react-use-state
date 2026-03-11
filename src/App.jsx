ec1c23717304876f5c3ed8d4d11d7263433a2c4f
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-11 13:00:29 +0100

    commit apertura esercizio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import AppHeader from './components/AppHeader'
+import AppMain from './components/AppMain'
+import AppFooter from './components/AppFooter'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <AppHeader/>
+      <AppMain/>
+      <AppFooter/>
+    </>
+  )
+}
+
+export default App
